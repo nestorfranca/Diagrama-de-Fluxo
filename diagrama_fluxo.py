7033a2477a7dd5f29a16713c39d124d8f54a4ba8
gustavo-a-mendes <gust4v0.a.mendes@gmail.com>
2024-10-05 00:39:40 -0300

    menu_provisório

--- diagrama_fluxo.py
+++ diagrama_fluxo.py
@@ -55,15 +55,11 @@ def exibe_menu():
 
     print('SISTEMAS DE CONTROLE')
     print('''
-1 - Selecionar Sinal
-2 - Adicionar Sinal
-3 - Adicionar Conexão
-4 - Listar Ganhos do Caminhos à Frente
-5 - Listar Ganhos de Laço
-6 - Desenhar Diagrama
-7 - Calcular FT
-8 - Ver Status
-9 - Encerrar
+1 - Ver Status
+2 - Listar Ganhos do Caminhos à Frente
+3 - Listar Ganhos de Laço
+4 - Calcular FT
+5 - Encerrar
     ''');
 
     return int(input('\nEscolha uma opção: '));
@@ -85,70 +81,35 @@ conex = input('Insira as conexões a serem adicionadas, separadas por vírgula:\
 
 sistema.adiciona_conexao(conex)
 
-
 opc = 0
 while True:
     opc = exibe_menu()
     
     if opc == 1:
-        print('Abrindo Lista de Sinais...'); time.sleep(0.2)
-        os.system('cls')        
-        # sistema.adiciona_conexao(1, 2)
-        sistema.listar_sinais()
-        # opc2 = menu_sinal()
-        input()
-
-    if opc == 2:
-        print('Adicionando Sinal...'); time.sleep(0.2)
-        os.system('cls')
-        sistema.adiciona_sinal(1)      
-        input()
-    
-    if opc == 3:
-        print('Adicionando Conexão...'); time.sleep(0.2)
-        os.system('cls')
-
-        print('''
-Para adicionar novas conexões, utilize a simbologia a seguir:
-
-    1>2: indica um caminho do sinal 1 para o sinal 2;
-    2>1: indica um laço de realimentação do sinal 2 para o sinal 1.
-
-        ''')
-        conex = input('Insira as conexões a serem adicionadas, separadas por vírgula:\n')
-        sistema.adiciona_conexao(conex)      
-    
-    if opc == 4:
-        print('Listando Ganhos do Caminho à Frente...'); time.sleep(0.2)
-        os.system('cls')
-        sistema.lista_caminhos()
-        input()
-    
-    if opc == 5:
-        print('Listando Ganhos de Laço...'); time.sleep(0.2)
-        os.system('cls')
-        sistema.lista_lacos()
-        input()
-
-    if opc == 6:
-        print('Desenhando Diagrama...'); time.sleep(0.2)
-        os.system('cls')
-        sistema.desenha_diagrama()
-        input()
-
-    if opc == 7:
-        print('Calculando Função de Transferência...'); time.sleep(0.2)
-        os.system('cls')
-        sistema.calcula_FT()
-        input()
-
-    if opc == 8:
         print('Vendo Status...'); time.sleep(0.2)
         os.system('cls')
         sistema.status()
         input()
 
-    if opc == 9:
+    if opc == 2:
+        print('Listando Ganhos do Caminho à Frente...'); time.sleep(0.2)
+        os.system('cls')
+        sistema.lista_caminhos()
+        input()
+    
+    if opc == 3:
+        print('Listando Ganhos de Laço...'); time.sleep(0.2)
+        os.system('cls')
+        sistema.lista_lacos()
+        input()
+
+    if opc == 4:
+        print('Calculando Função de Transferência...'); time.sleep(0.2)
+        os.system('cls')
+        sistema.calcula_FT()
+        input()
+
+    if opc == 5:
         print('Encerrando Programa...'); time.sleep(0.2)
         os.system('cls')
         break
